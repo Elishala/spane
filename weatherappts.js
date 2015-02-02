5b746cd72a8fb904ac1404805980ffc40eb462a1
Elishala <elishalahansen@gmail.com>
2015-02-02 15:08:03 -0500

    initial commit

---
+++ weatherappts.js
@@ -0,0 +1,8 @@
+$function (){
+ $('.spa-content').html('div class="panel-1"></div>');
+
+  $('.panel-1').click(function() {
+     $('.spa-content').html('<div class="panel-2"></div>');
+     });
+
+});
